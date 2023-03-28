3fdfef21fd19337e18b16ef1aa486aa51c2b1c7b
Arnau Claramunt <95536223+ArnauCS03@users.noreply.github.com>
2023-03-28 12:38:51 +0200

    Add files via upload

---
+++ Bloc2/s5/Descarregar tota Carpeta Patricio/ui_MyForm.h
@@ -0,0 +1,90 @@
+/********************************************************************************
+** Form generated from reading UI file 'MyForm.ui'
+**
+** Created by: Qt User Interface Compiler version 5.15.3
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MYFORM_H
+#define UI_MYFORM_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+#include "MyGLWidget.h"
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MyForm
+{
+public:
+    QHBoxLayout *horizontalLayout;
+    MyGLWidget *widget;
+    QVBoxLayout *verticalLayout;
+    QSpacerItem *verticalSpacer;
+    QPushButton *pushButton;
+
+    void setupUi(QWidget *MyForm)
+    {
+        if (MyForm->objectName().isEmpty())
+            MyForm->setObjectName(QString::fromUtf8("MyForm"));
+        MyForm->resize(819, 726);
+        horizontalLayout = new QHBoxLayout(MyForm);
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        widget = new MyGLWidget(MyForm);
+        widget->setObjectName(QString::fromUtf8("widget"));
+        QSizePolicy sizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(widget->sizePolicy().hasHeightForWidth());
+        widget->setSizePolicy(sizePolicy);
+        widget->setMinimumSize(QSize(50, 50));
+
+        horizontalLayout->addWidget(widget);
+
+        verticalLayout = new QVBoxLayout();
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        verticalLayout->addItem(verticalSpacer);
+
+        pushButton = new QPushButton(MyForm);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        QSizePolicy sizePolicy1(QSizePolicy::Fixed, QSizePolicy::Fixed);
+        sizePolicy1.setHorizontalStretch(0);
+        sizePolicy1.setVerticalStretch(0);
+        sizePolicy1.setHeightForWidth(pushButton->sizePolicy().hasHeightForWidth());
+        pushButton->setSizePolicy(sizePolicy1);
+
+        verticalLayout->addWidget(pushButton);
+
+
+        horizontalLayout->addLayout(verticalLayout);
+
+
+        retranslateUi(MyForm);
+        QObject::connect(pushButton, SIGNAL(clicked()), MyForm, SLOT(close()));
+
+        QMetaObject::connectSlotsByName(MyForm);
+    } // setupUi
+
+    void retranslateUi(QWidget *MyForm)
+    {
+        MyForm->setWindowTitle(QCoreApplication::translate("MyForm", "IDI-Lab", nullptr));
+        pushButton->setText(QCoreApplication::translate("MyForm", "&Sortir", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MyForm: public Ui_MyForm {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MYFORM_H
