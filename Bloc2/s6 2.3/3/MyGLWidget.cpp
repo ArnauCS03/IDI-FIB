2376ec6115d490c625940bb688d54bfe2e133b62
Arnau Claramunt <95536223+ArnauCS03@users.noreply.github.com>
2023-03-31 16:12:11 +0200

    Update MyGLWidget.cpp

--- Bloc2/s6 2.3/3/MyGLWidget.cpp
+++ Bloc2/s6 2.3/3/MyGLWidget.cpp
@@ -287,7 +287,7 @@ void MyGLWidget::mouseMoveEvent (QMouseEvent *event) {
     makeCurrent();
     if (ratoli.inici_accio) {
         if (!ratoli.invertir_moviment) {
-            cam3persona.psi -= (event->x() - ratoli.pos_inicial.x)/ratoli.sensitibity; // divirir tot entre 200, és un suavitzador
+            cam3persona.psi += (event->x() - ratoli.pos_inicial.x)/ratoli.sensitibity; // divirir tot entre 200, és un suavitzador
             cam3persona.theta -= (event->y() - ratoli.pos_inicial.y)/ratoli.sensitibity;
 
             if (not ratoli.modo_freestyle) {
@@ -297,7 +297,7 @@ void MyGLWidget::mouseMoveEvent (QMouseEvent *event) {
             
         }
         else { //[Opional] invertir moviment
-            cam3persona.psi += (event->x() - ratoli.pos_inicial.x)/ratoli.sensitibity; 
+            cam3persona.psi -= (event->x() - ratoli.pos_inicial.x)/ratoli.sensitibity; 
             cam3persona.theta += (event->y() - ratoli.pos_inicial.y)/ratoli.sensitibity;
 
             if (not ratoli.modo_freestyle) {
