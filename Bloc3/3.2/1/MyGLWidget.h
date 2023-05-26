a55e300b2a463af9787ab0389e7cdfb92fffb491
Arnau Claramunt <95536223+ArnauCS03@users.noreply.github.com>
2023-05-26 18:06:59 +0200

    Add files via upload

---
+++ Bloc3/3.2/1/MyGLWidget.h
@@ -0,0 +1,29 @@
+// MyGLWidget.h
+#include "BL3GLWidget.h"
+
+class MyGLWidget : public BL3GLWidget {
+  Q_OBJECT
+
+  public:
+    MyGLWidget(QWidget *parent=0) : BL3GLWidget(parent) {}
+    ~MyGLWidget();
+
+  protected:
+
+    virtual void initializeGL();
+
+    void iniFocus();
+
+    virtual void mouseMoveEvent(QMouseEvent *e);
+    virtual void keyPressEvent(QKeyEvent* event);
+
+    virtual void iniMaterialTerra();
+
+    glm::vec4 posFocus;
+    glm::vec3 colorFocus, llumAmbient;
+
+    GLuint colorFocusLoc, posFocusLoc, llumAmbientLoc;
+
+  private:
+    int printOglError(const char file[], int line, const char func[]);
+};
