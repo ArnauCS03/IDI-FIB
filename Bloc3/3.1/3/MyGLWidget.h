ed92b594f844a8f9efc5adc9a943f23566f7b805
Arnau Claramunt <95536223+ArnauCS03@users.noreply.github.com>
2023-05-26 13:29:53 +0200

    Add files via upload

---
+++ Bloc3/3.1/3/MyGLWidget.h
@@ -0,0 +1,19 @@
+// MyGLWidget.h
+#include "BL3GLWidget.h"
+
+class MyGLWidget : public BL3GLWidget {
+  Q_OBJECT
+
+  public:
+    MyGLWidget(QWidget *parent=0) : BL3GLWidget(parent) {}
+    ~MyGLWidget();
+
+  protected:
+    virtual void mouseMoveEvent(QMouseEvent *e);
+    virtual void keyPressEvent(QKeyEvent* event);
+
+    virtual void iniMaterialTerra();
+
+  private:
+    int printOglError(const char file[], int line, const char func[]);
+};
