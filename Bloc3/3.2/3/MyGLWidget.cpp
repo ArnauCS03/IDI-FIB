2a66cf11c9b8eb0549705a8696e7b109d97eb211
Arnau Claramunt <95536223+ArnauCS03@users.noreply.github.com>
2023-05-27 13:15:20 +0200

    Add files via upload

---
+++ Bloc3/3.2/3/MyGLWidget.cpp
@@ -0,0 +1,153 @@
+// MyGLWidget.cpp
+#include "MyGLWidget.h"
+#include <iostream>
+#include <stdio.h>
+
+#define printOpenGLError() printOglError(__FILE__, __LINE__)
+#define CHECK() printOglError(__FILE__, __LINE__,__FUNCTION__)
+#define DEBUG() std::cout << __FILE__ << " " << __LINE__ << " " << __FUNCTION__ << std::endl;
+
+int MyGLWidget::printOglError(const char file[], int line, const char func[]) 
+{
+    GLenum glErr;
+    int    retCode = 0;
+
+    glErr = glGetError();
+    const char * error = 0;
+    switch (glErr)
+    {
+        case 0x0500:
+            error = "GL_INVALID_ENUM";
+            break;
+        case 0x501:
+            error = "GL_INVALID_VALUE";
+            break;
+        case 0x502: 
+            error = "GL_INVALID_OPERATION";
+            break;
+        case 0x503:
+            error = "GL_STACK_OVERFLOW";
+            break;
+        case 0x504:
+            error = "GL_STACK_UNDERFLOW";
+            break;
+        case 0x505:
+            error = "GL_OUT_OF_MEMORY";
+            break;
+        default:
+            error = "unknown error!";
+    }
+    if (glErr != GL_NO_ERROR)
+    {
+        printf("glError in file %s @ line %d: %s function: %s\n",
+                             file, line, error, func);
+        retCode = 1;
+    }
+    return retCode;
+}
+
+MyGLWidget::~MyGLWidget() {
+}
+
+void MyGLWidget::initializeGL() {
+
+  BL3GLWidget::initializeGL();
+
+  posFocusLoc = glGetUniformLocation (program->programId(), "posFocusFS");
+  colorFocusLoc = glGetUniformLocation (program->programId(), "colorFocusFS");
+  llumAmbLoc = glGetUniformLocation (program->programId(), "llumAmbient");
+
+  iniFocus();
+}
+
+
+void MyGLWidget::iniFocus() {
+
+  focus_escena = true;
+  posFocusEscena = View * glm::vec4(1, 1, 1, 1); // Inicialment es focus d'Escena
+  colorFocus = glm::vec3(0.8, 0.8, 0.8);
+  llumAmbient = glm::vec3(0.2, 0.2, 0.2);
+
+
+  glUniform4fv (posFocusLoc, 1, &posFocusEscena[0]);
+  glUniform3fv (colorFocusLoc, 1, &colorFocus[0]);
+  glUniform3fv (llumAmbLoc, 1, &llumAmbient[0]);
+
+  posFocusCamera = glm::vec4(0, 0, 0, 1);
+}
+
+
+void MyGLWidget::mouseMoveEvent(QMouseEvent *e)
+{
+  makeCurrent();
+  // Aqui cal que es calculi i s'apliqui la rotacio o el zoom com s'escaigui...
+  if (DoingInteractive == ROTATE)
+  {
+    // Fem la rotació
+    angleY += (e->x() - xClick) * M_PI / ample;
+    viewTransform ();
+  }
+
+  xClick = e->x();
+  yClick = e->y();
+
+  update ();
+}
+
+void MyGLWidget::keyPressEvent(QKeyEvent* event) {
+  makeCurrent();
+
+  switch (event->key()) {
+
+    case Qt::Key_F:
+
+      if (focus_escena) {  // passem focus de camara
+        glUniform4fv (posFocusLoc, 1, &posFocusCamera[0]);
+        focus_escena = false;
+      }
+      else {      // passem focus d'escena
+        posFocusEscena = View * glm::vec4(1, 1, 1, 1);  
+        glUniform4fv (posFocusLoc, 1, &posFocusEscena[0]);
+        focus_escena = true;
+      }
+      break;
+
+    case Qt::Key_K: // moure focus Escena esquerra
+
+      posFocusEscena = posFocusEscena + glm::vec4(-0.2, 0, 0, 0);
+      glUniform4fv(posFocusLoc, 1, &posFocusEscena[0]);
+
+      break;
+
+    case Qt::Key_L: // moure focus Escena dreta
+
+      posFocusEscena = posFocusEscena + glm::vec4(0.2, 0, 0, 0);
+      glUniform4fv(posFocusLoc, 1, &posFocusEscena[0]);
+
+      break;
+
+
+    default: BL3GLWidget::keyPressEvent(event); break;
+  }
+  update();
+}
+
+void MyGLWidget::iniMaterialTerra() {
+
+  amb = glm::vec3(0,0,0.2);
+  diff = glm::vec3(0,0,0.8);
+  spec = glm::vec3(1,1,1);
+  shin = 100;
+}
+
+
+void MyGLWidget::viewTransform() {
+
+  BL3GLWidget::viewTransform();
+
+  if (focus_escena) {
+    posFocusEscena = View * glm::vec4(1, 1, 1, 1);  
+    glUniform4fv (posFocusLoc, 1, &posFocusEscena[0]);
+  }
+}
+
